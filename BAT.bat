885c364048cf6ff41bae6f7c1a20461338a7543f
vasek9876 <76277379+vasek9876@users.noreply.github.com>
2021-07-20 23:05:59 +0200

    1.0.0
    Obsahuje chybu, kdy kopíruje do složky i vnitřek...

--- BAT.bat
+++ BAT.bat
@@ -1,43 +1,57 @@
-@ECHO OFF
 
 ::----------------------------------------------------------------------------
 
 
-
 :: folder_location
-set A=test1
+set A=test\python39-32\lib\site-packages
 :: modules (folder/file.py)
-set A1=A
-set A2=x.py
-
+set A1=pyparsing
+set A2=yaml.py
 
 
 ::-----------------------------------------------------------------------------
-::internal
-set B=test2\package
-::delete __pycache__
-cd %A%
+
+:: vnitřní nastavení
+@ECHO OFF
+set MY=%CD%
+set B=package
+
+:: vytvoří adresář pro ukládání
+cd %MY%
+md %B%
+
+:: kopíruje
+
+:: 1. sobor/složka
+if NOT %A1:~-2,3% == py (
+md %B%\%A1%
+xcopy %A%\%A1% %B%\%A1% /s /e
+)
+else (
+xcopy %A%\%A1% %B%
+)
+
+:: 2. sobor/složka
+if NOT %A2:~-2,3% == py (
+md %B%\%A2%
+xcopy %A%\%A2% %B%\%A2% /s /e
+)
+else (
+xcopy %A%\%A2% %B%
+)
+
+:: smaže __pycache__
+cd %MY%\%B%
 dir /b __pycache__ /s > %temp%\files
-for /f %%a in (%temp%\files) do (echo rmdir /s /q %%a)
+for /f %%a in (%temp%\files) do (rmdir /s /q %%a)
 del %temp%\files
 
-::vytvoří adresář pro ukládání
-cd ..
-md test2
-md test2\%A1%
-md test2\%A2%
-
-::kopíruje
-if NOT %A1:~-2,3% == py (xcopy /e/s %A%\%A1% %B%\%A1%)
-::else (copy %A%\%A1% %B%\%A1:~0,-3%.py)
-::if NOT %A2:~-2,3% == py (xcopy /e/s %A%\%A2% %B%\%A2%)
-if %A2:~-2,3% == py (xcopy %A% %B%)
-
-
-if EXIST package.zip rmdir /s /q package.zip
-::"balí a uklízí"
+:: komprimuje
+cd %MY%
 for /D %%d in (%B%) do "C:\Program Files\7-Zip\7z.exe" a -tzip "package.zip" ".\%%d\*"
-rmdir /s /q test2
+
+:: maže kopírované soubory a složky
+rmdir /s /q %B%
 
 echo -----------------------------------------------------------------
 echo Done!
