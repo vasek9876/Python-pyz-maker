777781b74f8f68f7d8e13c9298ddb0177599c267
vasek9876 <76277379+vasek9876@users.noreply.github.com>
2021-07-29 20:33:10 +0200

    new version

--- compress.bat
+++ compress.bat
@@ -3,7 +3,7 @@
 REM ---- VSTUPNÍ PROMĚNNÉ ----
 set ModulesLocation=test\python39-32\lib\site-packages
 set Modules=pyparsing,yaml,x
-::set Adons=a
+set Adons=a
 set PyzName=package.zip
 REM -------------------------------------------
 
@@ -17,38 +17,39 @@ REM ---- vytvoří adresář pro komprimaci ----
 cd %MyLocation%
 md %MyPyzTemp%
 REM -------------------------------------------
-
+cls
+echo #kopirovani knihoven
 REM ---- CYKLUS ----
 for %%M in (%Modules%) do (
 	set x=%ModulesLocation%\%%M
 	if EXIST %ModulesLocation%\%%M.py (
-		xcopy /s /e %ModulesLocation%\%%M.py %MyPyzTemp% 
-	) else (
+		xcopy /q /s /e %ModulesLocation%\%%M.py %MyPyzTemp% 
+	) else if EXIST %ModulesLocation%\%%M (
 		md %MyPyzTemp%\%%M
-		xcopy /s /e %ModulesLocation%\%%M %MyPyzTemp%\%%M 
+		xcopy /q /s /e %ModulesLocation%\%%M %MyPyzTemp%\%%M 
 	)
 )
 REM -------------------------------------------
 
-echo #pycache
+echo #mazani pycache
 REM ---- smaže __pycache__ ----
 cd %MyPyzTemp%
 dir /b __pycache__ /s > %MyDataTemp%
 for /f %%A in (%MyDataTemp%) do (rmdir /s /q %%A)
-del %MyDataTemp%
+del /q %MyDataTemp%
 REM -------------------------------------------
 
 echo #komprese
 REM ---- smaže starý zip (pyz) a komprimuje----
 cd %MyLocation%
-if EXIST %PyzName% (del /s /q %PyzName%)
+if EXIST %PyzName% (del /q /s  %PyzName%)
 for /D %%d in (%MyPyzTemp%) do "C:\Program Files\7-Zip\7z.exe" a -tzip "%PyzName%" ".\%%d\*"
 REM -------------------------------------------
 
-
+echo #cisteni
 REM ---- smaže adresář pro komprimaci ----
 cd %MyLocation%
-rmdir /s /q %MyPyzTemp%
+rmdir /q /s  %MyPyzTemp%
 REM -------------------------------------------
 
 REM ---- Čekám na kliknutí a pak ukončím ----
